4356a96b2f998959a7ad935e6c3fd1b50286c59d
TsantaM <tsantamirindra.dev@gmail.com>
2025-08-17 15:06:15 +0300

    Create Gate Card First schema

---
+++ app/dashboard/game-designer/manage-gate-cards/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardHeader } from "@/components/ui/card"
+import Section from "@/components/ui/section"
+import { getUser, getUserRole } from "@/src/actions/getUserSession"
+import { Plus } from "lucide-react"
+import Link from "next/link"
+import { unauthorized } from "next/navigation"
+
+export default async function ManageGateCardsPage() {
+
+    const user = await getUser()
+    const role = await getUserRole()
+
+    if (!user || role?.role != 'GAMEDESIGNER') {
+        unauthorized()
+    }
+
+    return (
+        <>
+            <Section className="md:p-0">
+                <Card>
+                    <CardHeader>
+                        <div className="flex items-center justify-end gap-2">
+                            <Button variant='outline' asChild><Link href='/dashboard/game-designer/manage-gate-cards/create-gate-card'><Plus /> Add new Gate Card</Link></Button>
+                        </div>
+                    </CardHeader>
+                </Card>
+            </Section>
+        </>
+    )
+}
